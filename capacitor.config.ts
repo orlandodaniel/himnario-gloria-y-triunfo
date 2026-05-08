e21cd1746b4ea887ffe27bdf1a20a41ea9d8d641
Orlando Daniel Belisario <orlandobelisario1@gmail.com>
2026-05-08 14:51:15 -0400

    feat: add splash screen support and update app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,22 +9,11 @@ const config: CapacitorConfig = {
   },
   plugins: {
     SplashScreen: {
-      // Tiempo que se muestra la imagen (2-3 segundos es lo ideal)
       launchShowDuration: 2000, 
-      
-      // Se oculta automáticamente después del tiempo arriba definido
       launchAutoHide: true, 
-      
-      // Color de fondo del splash
       backgroundColor: "#1d4ed8", 
-      
-      // Cómo se ajusta la imagen a la pantalla
       androidScaleType: "CENTER_CROP", 
-      
-      // Evita que se vea un spinner genérico de Android sobre tu logo
       showSpinner: false,
-      
-      // Permite que el splash screen se desvanezca suavemente
       splashFullScreen: true,
       splashImmersive: true
     }
